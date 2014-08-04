d7bade3600dd90291eef63b51d2c25465cd134a0
Yoni116 <yoni116@gmail.com>
2014-08-04 22:29:38 +0300

    000

--- src/War.java
+++ src/War.java
@@ -7,91 +7,91 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class War extends Thread {
-    private String name;
-    private static Logger warLogger = Logger.getLogger("WarLogger");
-    private boolean status;
-    private List<MissileLauncher> missileLaunchers;
-    private List<IronDome> ironDomes;
-    private List<LauncherDestructor> launcherDestructors;
+	private String name;
+	private static Logger warLogger = Logger.getLogger("WarLogger");
+	private boolean status;
+	private List<MissileLauncher> missileLaunchers;
+	private List<IronDome> ironDomes;
+	private List<LauncherDestructor> launcherDestructors;
 
-    public War(String name, List<MissileLauncher> launchers,
-	    List<IronDome> ironDomes,
-	    List<LauncherDestructor> launcherDestructors) {
-	this.name = name;
-	this.missileLaunchers = launchers;
-	this.ironDomes = ironDomes;
-	this.launcherDestructors = launcherDestructors;
-	this.status = false;
-	warLogger.setUseParentHandlers(false);
-	warLogger.setLevel(Level.INFO);
-	try {
-	    File logdir = new File("logs");
-	    if (!logdir.exists())
-		logdir.mkdir();
-	    FileHandler fh = new FileHandler("logs/war_" + this.name + ".txt");
-	    fh.setFormatter(new WarFormatter());
-	    warLogger.addHandler(fh);
-	} catch (SecurityException e) {
-	    e.printStackTrace();
-	} catch (IOException e) {
-	    e.printStackTrace();
-	}
-    }
-
-    public War(String name) {
-	this.name = name;
-	this.missileLaunchers = new Vector<MissileLauncher>();
-	this.ironDomes = new Vector<IronDome>();
-	this.launcherDestructors = new Vector<LauncherDestructor>();
-    }
-
-    @Override
-    public void run() {
-	synchronized (this) {
-	    try {
-		wait();
-		status = true;
-	    } catch (InterruptedException e1) {
-		e1.printStackTrace();
-	    }
-	}
-	while (status) {
-	    try {
-		for (MissileLauncher missileLauncher : missileLaunchers) {
-		    missileLauncher.start();
+	public War(String name, List<MissileLauncher> launchers,
+			List<IronDome> ironDomes,
+			List<LauncherDestructor> launcherDestructors) {
+		this.name = name;
+		this.missileLaunchers = launchers;
+		this.ironDomes = ironDomes;
+		this.launcherDestructors = launcherDestructors;
+		this.status = false;
+		warLogger.setUseParentHandlers(false);
+		warLogger.setLevel(Level.INFO);
+		try {
+			File logdir = new File("logs");
+			if (!logdir.exists())
+				logdir.mkdir();
+			FileHandler fh = new FileHandler("logs/war_" + this.name + ".txt");
+			fh.setFormatter(new WarFormatter());
+			warLogger.addHandler(fh);
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		for (IronDome ironDome : ironDomes) {
-		    ironDome.start();
-		}
-		for (LauncherDestructor launcherDestructor : launcherDestructors) {
-		    launcherDestructor.start();
-		}
-	    } catch (IllegalThreadStateException e) {
-	    }
 	}
-    }
 
-    public void end() {
-	status = false;
-    }
+	public War(String name) {
+		this.name = name;
+		this.missileLaunchers = new Vector<MissileLauncher>();
+		this.ironDomes = new Vector<IronDome>();
+		this.launcherDestructors = new Vector<LauncherDestructor>();
+	}
 
-    public String getWarName() {
-	return name;
-    }
+	@Override
+	public void run() {
+		synchronized (this) {
+			try {
+				wait();
+				status = true;
+			} catch (InterruptedException e1) {
+				e1.printStackTrace();
+			}
+		}
+		while (status) {
+			try {
+				for (MissileLauncher missileLauncher : missileLaunchers) {
+					missileLauncher.start();
+				}
+				for (IronDome ironDome : ironDomes) {
+					ironDome.start();
+				}
+				for (LauncherDestructor launcherDestructor : launcherDestructors) {
+					launcherDestructor.start();
+				}
+			} catch (IllegalThreadStateException e) {
+			}
+		}
+	}
 
-    public boolean getStatus() {
-	return status;
-    }
+	public void end() {
+		status = false;
+	}
 
-    public List<IronDome> getIronDomes() {
-	return ironDomes;
-    }
+	public String getWarName() {
+		return name;
+	}
 
-    public List<LauncherDestructor> getLauncherDestructors() {
-	return launcherDestructors;
-    }
+	public boolean getStatus() {
+		return status;
+	}
 
-    public List<MissileLauncher> getMissileLaunchers() {
-	return missileLaunchers;
-    }
+	public List<IronDome> getIronDomes() {
+		return ironDomes;
+	}
+
+	public List<LauncherDestructor> getLauncherDestructors() {
+		return launcherDestructors;
+	}
+
+	public List<MissileLauncher> getMissileLaunchers() {
+		return missileLaunchers;
+	}
 }
