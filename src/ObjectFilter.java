444232be8b50019ac3e9b7273e86d97765b5422a
Muli Yulzary <muliyul@gmail.com>
2014-08-06 10:04:46 +0300

    test

--- src/ObjectFilter.java
+++ src/ObjectFilter.java
@@ -2,19 +2,19 @@ import java.util.logging.Filter;
 import java.util.logging.LogRecord;
 
 public class ObjectFilter implements Filter {
-    private Object o;
+	private Object o;
 
-    public ObjectFilter(Object o) {
-	this.o = o;
-    }
+	public ObjectFilter(Object o) {
+		this.o = o;
+	}
 
-    @Override
-    public boolean isLoggable(LogRecord rec) {
-	if (rec.getParameters() != null) {
-	    Object temp = rec.getParameters()[0];
-	    return o == temp;
-	} else
-	    return false;
-    }
+	@Override
+	public boolean isLoggable(LogRecord rec) {
+		if (rec.getParameters() != null) {
+			Object temp = rec.getParameters()[0];
+			return o == temp;
+		} else
+			return false;
+	}
 
 }
